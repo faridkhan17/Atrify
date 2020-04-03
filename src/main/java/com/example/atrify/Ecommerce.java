67e6dc2615483c029591417e2423b13fa858347d
farid_f06@yahoo.com <Imranpti12>
2020-04-03 14:29:10 +0200

    documentation

--- src/main/java/com/example/atrify/Ecommerce.java
+++ src/main/java/com/example/atrify/Ecommerce.java
@@ -7,13 +7,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import java.util.UUID;
-
 @SpringBootApplication
-public class AtrifyApplication {
+public class Ecommerce {
 
     public static void main(String[] args) {
-        SpringApplication.run(AtrifyApplication.class, args);
+        SpringApplication.run(Ecommerce.class, args);
     }
 
 
--- src/test/java/com/example/atrify/EcommerceTests.java
+++ src/test/java/com/example/atrify/EcommerceTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class AtrifyApplicationTests {
+class EcommerceTests {
 
     @Test
     void contextLoads() {
